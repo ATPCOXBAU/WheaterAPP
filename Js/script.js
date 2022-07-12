adb7605397f448d8105174b2250269f3906fe7a9
ATPCOXBAU <93449158+ATPCOXBAU@users.noreply.github.com>
2022-07-11 22:42:23 -0600

    integracion Mapas

--- Js/script.js
+++ Js/script.js
@@ -1,23 +1,45 @@
 
 const endpoint = "http://api.openweathermap.org/"
 const urlprueba = "https://api.openweathermap.org/data/2.5/onecall?lat=19.4285&lon=-99.1277&exclude=daily&appid=3d070b4fd285e9dfa261851a42575fbe&units=imperial"
-/*
 
 
-F7EBD4
 
 
-#F6A90E
+window.onload = function () {
 
- 
-#559C54
 
- 
-#990A94
+    var width = 3;
+    var styles = [
+        new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                color: 'white',
+                width: width + 2
+            })
+        }),
+        new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                color: 'blue',
+                width: width
+            })
+        })
+    ];
+    var map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([-99.1277, 19.4285,]),
+            zoom: 10
+        })
+    });
 
- 
-#0B330A
-*/
+}
+
+
+
 
 function apicall() {
     fetch(urlprueba)
@@ -32,10 +54,8 @@ function apicall() {
         })
 }
 
+
 function getlat() {
-
-
-
     let valor1 = prompt("Ingresa Tu Latitud", 0);
     console.log(valor1)
     let valor2 = prompt("Ingresa Tu Longitud", 0);
@@ -43,25 +63,3 @@ function getlat() {
 
 }
 
-
-function initMap() {
-
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: uluru,
-    });
-
-    const marker = new google.maps.Marker({
-        position: uluru,
-        map: map,
-    });
-}
-window.initMap = initMap;
-
-function load() {
-    alert("evento load detectado!");
-    initMap();
-}
-window.onload = load;
