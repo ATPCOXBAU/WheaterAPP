81efdee073110ba761f9bb06c1668a93c16607b5
ATPCOXBAU <93449158+ATPCOXBAU@users.noreply.github.com>
2022-12-29 21:09:27 -0600

    Cambios

--- Js/script.js
+++ Js/script.js
@@ -1,11 +1,19 @@
 
 const endpoint = "https://api.openweathermap.org/data/2.5/onecall"
-
+const endpoint1 = "https://api.openweathermap.org/data/2.5/forecast"
 window.onload = function () {
 
     gethour()
     const d = new Date()
-    let hora = document.getElementById("hour").textContent = "Hora " + d.getHours() + ":" + d.getMinutes()
+
+    if (d.getMinutes() < 10) {
+        mins = "0" + d.getMinutes()
+    }
+    else {
+        mins = d.getMinutes()
+    }
+
+    document.getElementById("hour").textContent = "Hora " + d.getHours() + ":" + mins
 
     navigator.geolocation.getCurrentPosition(success, error, options);
     console.log()
@@ -37,18 +45,25 @@ function gethour() {
 
 function apicall(lat, long) {
     var links = new URL(endpoint)
-    var params = { lat: lat, lon: long, appid: "3d070b4fd285e9dfa261851a42575fbe", units: "metric" }
+    var params = { lang: "es", lat: lat, lon: long, appid: "3d070b4fd285e9dfa261851a42575fbe", units: "metric" }
     links.search = new URLSearchParams(params).toString();
     fetch(links)
         .then(response => { return response.json() })
         .then(data => {
             console.log(data)
             console.log(data.current.temp)
+            console.log(data.daily)
+            mins = data.daily[0].temp.min
+
             document.getElementById("lugar").textContent = data.timezone
             document.getElementById("grados").textContent = Math.floor(data.current.temp) + "°C"
-            document.getElementById("estado").textContent = "Viento: " + Math.floor(data.current.wind_speed) + " m/s"
+            document.getElementById("estado").textContent = "Viento: " + (data.current.wind_speed) + " m/s"
             document.getElementById("humedad").textContent = "Humedad: " + Math.floor(data.current.humidity) + "%"
+            document.getElementById("estado2").textContent = "Estado: " + capitalizeFirstLetter(data.current.weather[0].description)
+            document.getElementById("Max").textContent = "Max: " + Math.floor(data.daily[0].temp.max) + "°C" + " Min: " + Math.floor(data.daily[0].temp.min) + "°C"
+
             map(lat, long)
+            dailyapicall(lat, long)
 
         })
         .catch(error => {
@@ -70,6 +85,10 @@ function map(lat, lon) {
         })
     });
 
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 
@@ -96,3 +115,21 @@ function error(err) {
 };
 
 
+function dailyapicall(lat, long) {
+    console.log(endpoint1)
+    var links = new URL(endpoint1)
+
+    var params = { lang: "es", lat: lat, lon: long, appid: "3d070b4fd285e9dfa261851a42575fbe", units: "metric" }
+    links.search = new URLSearchParams(params).toString();
+    fetch(links)
+        .then(response => { return response.json() })
+        .then(data => {
+            console.log(data)
+
+
+        })
+        .catch(error => {
+            console.log(error)
+        })
+
+}
